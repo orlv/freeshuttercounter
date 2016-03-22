dbb4602c0ba140a1dbc77daa83aaec4918c1e82a
Oleg Orlov <oleg.orlv@gmail.com>
2016-03-22 14:58:46 +0300

    added license

--- freeshuttercounter/freeshuttercounter.m
+++ freeshuttercounter/freeshuttercounter.m
@@ -5,6 +5,19 @@
 //  Created by oleg on 20.03.16.
 //  Copyright © 2016 Oleg Orlov. All rights reserved.
 //
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http:www.gnu.org/licenses/>.
+//
 
 #import <Foundation/Foundation.h>
 #import <Appkit/NSRunningApplication.h>
@@ -50,8 +63,6 @@ NSString *camera_get_config(Camera *camera, GPContext *context, const char *key)
 	gp_widget_free(widget);
 	return out;
 }
-
-
 
 /* Kill the process PTPCamera  */
 void kill_PTPCamera()
